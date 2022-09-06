6eae3b8d537b15f00dae83f2fb7dd90cf1befbe2
GERARD EMMANUEL <geremmanuel9@gmail.com>
2022-09-06 11:52:10 -0500

    Repositorio, servicios y controladores funcionando de la entidad employee

--- 1_base/Sprint_3_4/src/main/java/com/GADJET/Sprint_3_4/entities/Employee.java
+++ 1_base/Sprint_3_4/src/main/java/com/GADJET/Sprint_3_4/entities/Employee.java
@@ -1,4 +1,88 @@
 package com.GADJET.Sprint_3_4.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+
 public class Employee {
+
+    //Atributos y columnas de entidades
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "enterprise")
+    private String enterprise;
+
+    private enum role{
+        Manager,
+        Operative;
+    }
+
+    @Column(name = "role")
+    private role roleName;
+
+    // Constructor vacio
+    public Employee() {
+    }
+
+    // Constructor con parametros
+    public Employee(long id, String name, String email,String enterprise, role roleName) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.enterprise = enterprise;
+        this.roleName = roleName;
+    }
+
+    // getters and setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(String enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public role getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(role roleName) {
+        this.roleName = roleName;
+    }
 }
--- 1_base/Sprint_3_4/src/main/java/com/GADJET/Sprint_3_4/repositories/employeeRepository.java
+++ 1_base/Sprint_3_4/src/main/java/com/GADJET/Sprint_3_4/repositories/employeeRepository.java
@@ -1,4 +1,7 @@
 package com.GADJET.Sprint_3_4.repositories;
 
-public interface employeeRepository {
+import com.GADJET.Sprint_3_4.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface employeeRepository extends JpaRepository<Employee, Long> {
 }
